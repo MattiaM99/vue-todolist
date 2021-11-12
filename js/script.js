bb3660271b8da025b5b29015290cfb6cfb7ff940
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-12 15:05:30 +0100

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+  
+  el: "#app",
+
+  data: {
+    todoList:[
+      "fare la spesa",
+      "portare fuori il cane",
+      "andare in palestra"
+    ],
+
+    error: false,
+
+    stringToDo: "",
+  }
+})
